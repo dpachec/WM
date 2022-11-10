10af8a6ddfe019536c20b6ce678c7792e5fc502d
Daniel Pacheco <dapachec@gmail.com>
2022-11-10 14:21:00 +0100

    update dynamicity

--- dynamicity.m
+++ dynamicity.m
@@ -1,4 +1,6 @@
 %%
+%%
+
 clear, clc
 
 region = 'vvs';
@@ -25,7 +27,12 @@ for i = 1:length(out_c)
 end
 
 
-%% ENCODING 
+%% Compute dynamicity scores at the group level 
+clearvars -except DISC_EE DIDC_EE out_c
+
+
+
+timeL =  [51:150]; 
 
 
 meanC1 = cellfun(@(x) squeeze(mean(x)), DISC_EE, 'un', 0); 
@@ -38,11 +45,12 @@ mC1FD = cat(3, mC1F{:}); mC1FD = reshape(mC1FD, [lM lM nSubj]); mC1FD = permute(
 mC2FD = cat(3, mC2F{:}); mC2FD = reshape(mC2FD, [lM lM nSubj]); mC2FD = permute(mC2FD, [3 1 2]);
 
 mDiff = mC1FD - mC2FD; 
+mDiff = mDiff(:,timeL, timeL);
 
 
-for timei = 1:lM
+for timei = 1:size(mDiff, 2)
 
-    for timej = 1:lM
+    for timej = 1:size(mDiff, 2)
 
         if timei~=timej
             offDiag = mDiff(:, timei, timej); 
@@ -74,7 +82,7 @@ clustinfo = bwconncomp(dynMatrx);
 %% 
 figure; 
 %imagesc(dynMatrx)
-contourf(dynMatrx); 
+contourf(dynMatrx); axis square
 
 %% 
 d2p = mean(dynMatrx, 1); 
@@ -90,7 +98,7 @@ plot(d2p)
 
 nPerm = 100;
 
-clear clusL dynMatrxPerm mDiffPerm
+clear dynMatrxPerm mDiffPerm
 
 for permi = 1:nPerm
     
@@ -105,14 +113,14 @@ for permi = 1:nPerm
     mC2FD = cat(3, mC2F{:}); mC2FD = reshape(mC2FD, [lM lM nSubj]); mC2FD = permute(mC2FD, [3 1 2]);
 
     mDiff = mC1FD - mC2FD; 
-    
+    mDiff = mDiff(:, 1:15, 1:15);
 
     % % % shuffle matrix for all subjects 
     for subji = 1:nSubj
         mDiffSubj = squeeze(mDiff(subji, :, :)); 
         onDiag = diag(mDiffSubj); 
-        for timei = 1:lM
-            idR = randi(lM); 
+        for timei = 1:size(mDiff, 2)
+            idR = randi(size(mDiff, 2)); 
             tmp = mDiffSubj(timei, idR); 
             onDiagPerm(timei) = tmp; 
             mDiffSubj(timei, idR) = diag(timei); 
@@ -122,9 +130,9 @@ for permi = 1:nPerm
         mDiffPerm(subji, :, :) = mDiffSubj ;
     end
     
-    for timei = 1:lM
+    for timei = 1:size(mDiff, 2)
     
-        for timej = 1:lM
+        for timej = 1:size(mDiff, 2)
     
             if timei~=timej
     
@@ -155,15 +163,11 @@ end
 
 
 %% 
-d2p = squeeze(dynMatrxPerm(5,:,:));
+d2p = squeeze(dynMatrxPerm(22,:,:));
 figure
 contourf(d2p)
 clustinfo = bwconncomp(d2p)
 
-%%
-d2p = squeeze(dynMatrxPerm);
-figure
-imagesc(d2p)
 
 
 
@@ -175,7 +179,226 @@ imagesc(d2p)
 
 
 
-%%
+%% Compute dynamicity scores at the trial level 
+
+clearvars -except DISC_EE DIDC_EE out_c
+
+timeL = [1:150]; 
+
+C1 = DISC_EE; 
+C2 = DIDC_EE; 
+
+
+for subji = 1:length(C1)
+
+    clear clustL
+    C1Sub = C1{subji}; 
+    C2Sub = C2{subji}; 
+
+    % % % compute mean C2 and substract same mean matrix to all trials ? ? ? 
+    meanC2 = mean(C2Sub); 
+    nTrials = size(C1Sub, 1); 
+    meanC2 = repmat(meanC2, nTrials, 1, 1);
+    C1Sub = C1Sub - meanC2; 
+
+    for triali = 1:size(C1Sub, 1)
+
+        C1ST = squeeze(C1Sub(triali, :, :));
+        C1ST =  triu(C1ST.',1) + tril(C1ST ); 
+        C1ST = C1ST(timeL, timeL);
+           
+        for timei = 1:size(C1ST, 2)
+        
+            for timej = 1:size(C1ST, 2)
+        
+                if timei~=timej
+                    offDiag = C1ST(timei, timej); 
+                    onDiag1 = C1ST(timei, timei); 
+                    onDiag2 = C1ST(timej, timej); 
+                    h1 = onDiag1 > offDiag;
+                    h2 = onDiag2 > offDiag; 
+                    
+                    if h1 & h2 
+                        dynMatrx(timei, timej) = 1; 
+                    else
+                        dynMatrx(timei, timej) = 0; 
+                    end
+                end
+            end
+        end
+        
+        
+        clustinfo = bwconncomp(dynMatrx);
+        biggestClust = max(cellfun(@length, clustinfo.PixelIdxList)); 
+        if ~isempty(biggestClust)
+            clustL(triali,:) = biggestClust; 
+        else
+            clustL(triali,:) = 0; 
+        end
+    
+        dynMTrial(triali, : ,:) = dynMatrx; 
+    end
+
+    allDynMatrix{subji,:} = dynMTrial; 
+
+    allSClust{subji,:} = clustL;
+
+end
+
+%% 
+
+meanDYM = cellfun(@(x) squeeze(mean(x)), allDynMatrix, 'un', 0); 
+nSubj = size(meanDYM, 1);
+lM = size(meanDYM{1},1 ); 
+mDYMS = cat(3, meanDYM{:}); mDYMS = reshape(mDYMS, [lM lM nSubj]); mDYMS = permute(mDYMS, [3 1 2]);
+
+d2p = squeeze(mean(mDYMS))
+
+%imagesc(d2p); axis square
+
+d2p(triu(d2p) == 0) = nan
+contourf(d2p, 100, 'linecolor', 'none'); axis square; colorbar
+set(gca, 'clim', [.2 .45]); 
+
+%% 
+
+[h p ci ts] = ttest(mDYMS); 
+h = squeeze(h); t = squeeze(ts.tstat);
+imagesc(h); axis square; colorbar
+
+
+%% 
+contourf(meanDYM{6}); axis square; colorbar
+
+
+
+%% compute PS (on diagonal, off diagonal , full matrix) 
+
+clearvars -except DISC_EE DIDC_EE out_c allSClust
+
+timeL = [6:15]; 
+
+C1 = DISC_EE; 
+
+for subji = 1:size(C1, 1)
+    
+    clear psT
+
+    C1Sub = C1{subji}; 
+    
+    for triali = 1:size(C1Sub, 1)
+
+        C1ST = squeeze(C1Sub(triali, :, :));
+
+        % % % entire matrix 
+%         C1ST =  triu(C1ST.',1) + tril(C1ST ); 
+%         C1ST = C1ST(timeL, timeL);
+%         psT(triali, :) = mean(C1ST, 'all'); 
+
+        % % % % only off-diagonal
+        C1ST = squeeze(C1Sub(triali, timeL, timeL));
+        C1ST(eye(size(C1ST))>0) = nan; 
+        psT(triali, :) = mean(C1ST, 'all', 'omitnan'); 
+
+        
+        % % % only diagonal
+        %C1STD = diag(C1ST(timeL, timeL)); % restricted to the period of significant dynamicity
+        %psT(triali, :) = mean(C1STD); 
+
+    end
+
+    allPST{subji,:} = psT; 
+
+
+end
+
+
+
+%% correlate dynamicity with PS
+
+
+for subji = 1:size(allPST, 1)
+
+    ps = allPST{subji}; 
+    dy = allSClust{subji}; 
+
+
+    allR(subji) = corr(ps, dy, 'type', 's');
+
+
+end
+
+
+[h p ci ts] = ttest(allR); 
+obsT = ts.tstat
+
+
+
+%% plot one bar
+clear data
+ylim = [-1 1];
+
+
+data.data = allR';
+figure(); set (gcf, 'Position', [300 300 520 650]);
+mean_S = mean(data.data, 1);std_S = std(data.data, [], 1); h = bar (mean_S);hold on;
+hb = plot ([1], data.data); hold on; 
+set(hb, 'Marker', '.', 'MarkerSize',60);hold on;
+set(h,'FaceColor', 'none', 'lineWidth', 3);set(hb,'linestyle','none', 'lineWidth', 3);
+set(gca,'XTick',[1],'XTickLabel',{''}, 'FontSize', 30, 'linew',2, 'ylim', ylim, 'xlim', [0 2]);
+plot(get(gca,'xlim'), [0 0],'k','lineWidth', 3);
+ylabel('Rho')
+%exportgraphics(gcf, 'myP.png', 'Resolution', 300)
+
+
+
+
+
+%% compute after shuffling the trial labels 
+
+nPerm = 100
+
+for permi = 1:nPerm
+
+    for subji = 1:size(allPST, 1)
+        ps = allPST{subji}; 
+        dy = allSClust{subji}; 
+    
+        psPerm = ps(randperm(length(ps))); 
+        allRPerm(subji) = corr(psPerm, dy, 'type', 's'); 
+        
+    end
+    [h p ci ts] = ttest(allRPerm); 
+    allTPerm(permi,:) = ts.tstat; 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
